6e058d0d01a9b9325b6264ecd0c68d54684cf7bc
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2020-11-10 08:30:44 +0100

    added local storage

--- form/form.js
+++ form/form.js
@@ -1,7 +1,19 @@
 'use strict'
 
+window.addEventListener('load', start);
+
+function start(){
+  const btn = document.querySelector('button').textContent = 'Download'
+
+if(localStorage.getItem('form-submitted-before') === 'true'){
+  location.assign('/Asset/asset.html');
+}
+}
+
 const url = 'https://frontend2020-6cfe.restdb.io/rest/dxc-project';
 const key = '5f96ac774b77c1637d147e05';
+document.querySelector('form').setAttribute('novalidate', true);
+
 
 //post new data
 function post(data) {
@@ -19,20 +31,35 @@ function post(data) {
     .then(data => console.log(data));
 }
 
-document.querySelector('form').addEventListener('submit', (e)=>{
-e.preventDefault();
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
 
-const form = document.querySelector('form').elements;
+  const form = document.querySelector('form');
 
-const data = {
-full_name: form.full_name.value,
-email: form.email.value,
-company: form.company.value,
-job_title:form.job_title.value,
-country: form.country.value,
-consent: form.consent.checked};
+  const data = {
+    full_name: form.elements.full_name.value,
+    email: form.elements.email.value,
+    company: form.elements.company.value,
+    job_title: form.elements.job_title.value,
+    country: form.elements.country.value,
+    consent: form.elements.consent.checked
+  };
 
-post(data);
+  //if form is valid do this
+  if (form.reportValidity()) {
+    post(data);
 
+    setTimeout(() => {
+      location.assign('/Asset/asset.html');
+    }, 2000);
+
+    localStorageFn()
+  }
 });
+
+
+function localStorageFn() {
+  localStorage.setItem('form-submitted-before', true);
+  const btn = document.querySelector('button').textContent = 'Success'
+}
 
